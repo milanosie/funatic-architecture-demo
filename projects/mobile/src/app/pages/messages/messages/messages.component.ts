396b023c5696a728f8d95161d442b2eda3107c1d
milansteenwinkel <milan.steenwinkel@incentro.com>
2022-02-14 09:37:27 +0100

    Add messages

--- projects/mobile/src/app/pages/messages/messages/messages.component.ts
+++ projects/mobile/src/app/pages/messages/messages/messages.component.ts
@@ -9,15 +9,22 @@ import {Observable} from 'rxjs';
 })
 export class MessagesComponent implements OnInit {
 
+    private amount = 40;
     public filteredMessages = [];
     public messages = [];
+    private searchTerm = '';
+
     constructor(private messageService: MessageService) {
     }
 
     ngOnInit(): void {
+        this.getMessages();
+    }
+
+    private getMessages(): void {
         this.messageService.getMessages().subscribe((value) => {
             this.messages = value;
-            this.filteredMessages = this.messages;
+            this.filteredMessages = this.messages.filter((v) => v.id < this.amount);
         });
     }
 
@@ -26,7 +33,24 @@ export class MessagesComponent implements OnInit {
     }
 
     filterMessage(event: any): void {
-        console.log(event);
-        this.filteredMessages = this.messages.filter((v) => v.title.indexOf(event.detail.value) !== -1);
+        this.searchTerm = event.detail.value;
+        this.filteredMessages = this.messages.filter((v) => v.title.indexOf(this.searchTerm) !== -1 && v.id < this.amount);
+    }
+
+    loadMoreData($event: any): void {
+        setTimeout(() => {
+            this.amount += 10;
+            this.filteredMessages = this.messages.filter((v) => v.title.indexOf(this.searchTerm) !== -1 && v.id < this.amount);
+            $event.target.complete();
+        }, 1000);
+    }
+
+    doRefresh($event: any): void {
+        setTimeout(() => {
+            this.amount = 40;
+            this.searchTerm = '';
+            this.getMessages();
+            $event.target.complete();
+        }, 1000);
     }
 }
